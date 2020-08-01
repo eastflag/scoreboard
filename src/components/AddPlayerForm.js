020aba0f2f5118a3233029b3030d01d3079ffe30
react teacher <eastflag@gmail.com>
2020-08-01 13:09:39 +0900

    AddPlayerForm - validation

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -10,17 +10,30 @@ export class AddPlayerForm extends React.Component {
   }
 
   handleSubmit = (e) => {
-    console.log('handleSubmit');
     e.preventDefault();
+
+    // 폼 유효성 검증
+    const player = document.getElementById("player");
+    const form = document.getElementById("form");
+
+    console.log(player.validity.valid);
+    console.log(form.checkValidity());
+
+    if (!form.checkValidity()) {
+      // 에러문구를 띄우고
+      return;
+    }
+
     // name을 부모에게 넘겨서 부모가 player에 추가
     this.props.addPlayer(this.state.value);
   }
 
   render() {
     return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <input className="input" type="text" placeholder="enter a plaeyr's name"
-          value={this.state.value} onChange={this.handleValueChange}/>
+      <form id="form" className="form" onSubmit={this.handleSubmit} noValidate>
+        <input id="player" className="input" type="text" placeholder="enter a plaeyr's name"
+          value={this.state.value} onChange={this.handleValueChange}
+          required />
         <input className="input" value="Add Player" type="submit"/>
       </form>
     );
